7a3362b125ed74ba99f157e369a76afd2ba39ca7
Caleb <caleb@calebcurry.com>
2023-10-14 07:15:24 -0400

    Basic MVC structure

---
+++ src/controllers/users.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+import User from '../models/users.js';
+
+const getMany = async (req: Request, res: Response) => {
+    const users = await User.getMany();
+    res.render('users', { users });
+};
+
+const get = async (req: Request, res: Response) => {
+    const user = await User.get(Number.parseInt(req.params.id));
+    res.render('user', { user });
+};
+
+export default { getMany, get };
