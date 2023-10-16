6d5a67851c66292ccf6442dfac5161e8da80417e
Caleb <caleb@calebcurry.com>
2023-10-16 08:59:49 -0400

    ajv

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,15 +1,46 @@
 import { Request, Response, NextFunction } from 'express';
+import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
+import { betterAjvErrors } from '@apideck/better-ajv-errors';
 
-const validateUsername = (req: Request, res: Response, next: NextFunction) => {
-    const username = req?.body?.username;
+const ajv = new Ajv();
+addFormats(ajv);
 
-    if (!username || username.length < 5 || username.length > 30) {
-        return res.status(400).json({
-            error: 'Username must be 5-30 characters long',
+const schema = {
+    type: 'object',
+    properties: {
+        username: {
+            type: 'string',
+            minLength: 5,
+            maxLength: 30,
+        },
+        email: {
+            type: 'string',
+            format: 'email',
+        },
+    },
+    required: ['username', 'email'],
+};
+
+const validate = ajv.compile(schema);
+
+const validateAccount = (req: Request, res: Response, next: NextFunction) => {
+    const valid = validate(req.body);
+
+    if (!valid) {
+        const betterErrors = betterAjvErrors({
+            schema,
+            data: req.body,
+            errors: validate.errors,
+            basePath: '',
         });
-    }
+        res.status(400).json({
+            error: betterErrors,
+        });
 
+        return;
+    }
     next();
 };
 
-export default { validateUsername };
+export default { validateAccount };
