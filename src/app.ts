794f55e9348962814d84bc11c2aa670dfe71f28b
Caleb <caleb@calebcurry.com>
2023-10-16 06:35:09 -0400

    error handling w async route handlers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import usersRouter from './routes/users.js';
 import logging from './middleware/logging.js';
+import errors from './middleware/errors.js';
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use(logging.logRequest);
 app.use('/users', usersRouter);
+app.use(errors.errorHandler);
 
 app.listen(port, () => {
     console.log(`App listening http://localhost:${port}.`);
