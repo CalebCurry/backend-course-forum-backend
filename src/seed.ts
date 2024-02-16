d56dce36c946054e7504b044a0d31811933a67db
Caleb Curry <caleb@calebcurry.com>
2024-02-16 10:39:12 -0500

    modify seed to be idempotent

--- src/seed.ts
+++ src/seed.ts
@@ -3,16 +3,26 @@ import bcrypt from 'bcrypt';
 
 const password = await bcrypt.hash('admin369!', 10);
 
-await prisma.user.create({
-    data: {
-        name: 'admin',
-        username: 'admin',
-        email: 'admin@admin.com',
-        password: {
-            create: {
-                hash: password,
-            },
+const adminUser = await prisma.user.findFirst({
+    where: {
+        roles: {
+            has: 'ADMIN',
         },
-        roles: ['ADMIN'],
     },
 });
+
+if (!adminUser) {
+    await prisma.user.create({
+        data: {
+            name: 'admin',
+            username: 'admin',
+            email: 'admin@admin.com',
+            password: {
+                create: {
+                    hash: password,
+                },
+            },
+            roles: ['ADMIN'],
+        },
+    });
+}
